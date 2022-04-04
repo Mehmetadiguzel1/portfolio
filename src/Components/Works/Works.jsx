2a74332b08747dde5a040a660d4d27de7ed35711
Mehmet Furkan Adiguzel <55146992+Mehmetadiguzel1@users.noreply.github.com>
2022-04-04 18:14:53 -0400

    Works Side done

--- src/Components/Works/Works.jsx
+++ src/Components/Works/Works.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
 import './Works.css'
+import Facebook from '../../img/facebook.png';
+import AppStore from '../../img/app-store.png';
+import Chrome from '../../img/chrome.png';
+import Xbox from '../../img/xbox.png';
+import Starbucks from '../../img/Starbucks.png'
+
 const Works = () => {
   return (
-    <div>Works</div>
+    <div className="works">
+         <div className="awesome">
+            <span>Work for All these</span>
+            <span>Brand & Clients</span>
+            <span>I can give you all service for front end.
+            <br />
+            Hello1
+            <br />
+            Hello2
+            <br />
+            Hello3
+                
+            </span>
+            <button className='button s-button' > Dowload CV </button>
+
+            <div className='blur s-blur1' style={{background: 'ABF1FF94', }} ></div>
+            
+        </div>
+        <div className="w-right">
+            <div className="w-mainCircle">
+                <div className="w-secCircle">
+                    <img src={AppStore} alt="" />
+                </div>
+                <div className="w-secCircle">
+                    <img  src={Chrome} alt="" />
+                </div>
+                <div className="w-secCircle">
+                    <img  src={Xbox} alt="" />
+                </div>
+                <div className="w-secCircle">
+                    <img  src={Facebook} alt="" />
+                </div>
+                <div className="w-secCircle">
+                    <img  src={Starbucks} alt="" />
+                </div>
+            </div>
+
+            <div className='w-backCircle blueCircle' ></div>
+            <div className='w-backCircle yellowCircle' ></div>
+        </div>
+    </div>
   )
 }
 
