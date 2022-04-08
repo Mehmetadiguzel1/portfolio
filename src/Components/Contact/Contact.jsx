c622c25eeeec63c9b265aef427096c518379ae07
Mehmet Furkan Adiguzel <55146992+Mehmetadiguzel1@users.noreply.github.com>
2022-04-08 10:42:27 -0400

    Email contact done with emailjs

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
 import './Contact.css'
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 const Contact = () => {
+
+
+    const form = useRef();
+    const [ done, setDone ] = useState(false)
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_qj00cuy', 'template_45tgkmq', form.current, 'InX4ZiW4VH-8mThzz')
+        .then((result) => {
+            console.log(result.text);
+            setDone(true);
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+
+
   return (
     <div className="contact-form">
         <div className="w-left">
@@ -11,12 +30,14 @@ const Contact = () => {
             </div>
         </div>
         <div className="c-right">
-            <form>
+            <form ref={form} onSubmit={sendEmail} >
                 <input type="text" name='user_name' className='user' placeholder='Name' />
                 <input type="text" name='user_email' className='user' placeholder='Email' />
                 <textarea name="message" className='user' placeholder='Message' />
                 <input type="submit" value="Send" className="button" />
+                <span>{done && "Thans for contating me!"}</span>
                 <div className='blur c-blur1' style={{background: "rgb(252, 145, 252)"}} ></div>
+
             </form>
         </div>
     </div>
